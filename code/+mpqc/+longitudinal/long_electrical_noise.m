b2b1f84ef75a1a3725b9a973ad28b90651d0866c
Isabell Whiteley <46966503+iwhiteley@users.noreply.github.com>
2025-08-15 16:24:51 +0100

    Code not working, debugging

--- code/+mpqc/+longitudinal/long_electrical_noise.m
+++ code/+mpqc/+longitudinal/long_electrical_noise.m
@@ -1,21 +1,54 @@
-function long_electrical_noise(data_dir)
+function [d,out] = long_electrical_noise(data_dir)
 
 
 
 if nargin<1
     data_dir = pwd;
 end
-d = dir(data_dir);
+% d = dir(fullfile(data_dir,'**\*.*')%,contains('electrical_noise'));
+d = dir(fullfile(data_dir))%,'**\*.*')%,contains('electrical_noise'));
+d(~[d.isdir]) = []; % removes the non directories
 
-
-n=1;
+subD = cell(length(d));
 for ii=1:length(d)
-    tmp = d(ii);
-    if contains(tmp.name,'electrical_noise')
-        out(n) = generic_generator_template(tmp);
+    % tmp = d(ii);
+    subDir = d(ii).name;
+    subD{ii} = dir(fullfile(subDir));%, contains('electrical_noise'));
+    if contains(subD{ii}.name, 'electrical_noise')
         out(n).type = 'electrical_noise';
         out(n).plotting_func = @mpqc.plot.electrical_noise;
-        n=n+1;
     end
+    % dir(data_dir,d.name)
+% if contains(d.name,'electrical_noise')
+%     disp(d.name)
+% end
 end
+% n =1;
+% if contains(d.name, 'electrical_noise')
+%     out(n).type = 'electrical_noise';
+%     out(n).plotting_func = @mpqc.plot.electrical_noise;
+% end
+
+n=1;
+% for ii=1:length(d)
+%     tmp = d(ii);
+%     if contains(tmp.name,'electrical_noise')
+%         % out(n) = generic_generator_template(tmp);
+%         out(n).type = 'electrical_noise';
+%         out(n).plotting_func = @mpqc.plot.electrical_noise;
+%         n=n+1;
+%     else
+%         disp('No files')
+%     end
+% end
+
+% t_im = single(imstack(:,:,ii));
+%         [n,x] = hist(t_im(:),100);
+%         a=area(x,n);
+% 
+% 
+%         a.EdgeColor=[0,0,0.75];
+%         a.FaceColor=[0.5,0.5,1];
+%         a.LineWidth=2;
+
 end
---
+++ code/testscript.m
@@ -0,0 +1,2 @@
+a = 1
+[d,out] = mpqc.longitudinal.long_electrical_noise('C:\Users\Isabell\Desktop\2p-313_diagnostics\')
