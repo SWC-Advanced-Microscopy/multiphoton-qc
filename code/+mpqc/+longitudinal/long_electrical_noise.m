bdca4423ea36295c9f03460a060c1d9b3696a678
Isabell Whiteley <46966503+iwhiteley@users.noreply.github.com>
2025-08-18 11:41:03 +0100

    Can give the microscope diagnostics file and code identifies all noise measurement files and reads the noise tif file to the workspace. Currently also prints the noise data in command window

--- code/+mpqc/+longitudinal/long_electrical_noise.m
+++ code/+mpqc/+longitudinal/long_electrical_noise.m
@@ -1,47 +1,38 @@
-function [d,out] = long_electrical_noise(data_dir)
+function [d,out,noiseData] = long_electrical_noise(data_dir)
 
 
 
 if nargin<1
     data_dir = pwd;
 end
-% d = dir(fullfile(data_dir,'**\*.*')%,contains('electrical_noise'));
-d = dir(fullfile(data_dir))%,'**\*.*')%,contains('electrical_noise'));
-d(~[d.isdir]) = []; % removes the non directories
 
-subD = cell(length(d));
+d = dir(fullfile(data_dir,'\**'));
+n=1;
 for ii=1:length(d)
-    % tmp = d(ii);
-    subDir = d(ii).name;
-    subD{ii} = dir(fullfile(subDir));%, contains('electrical_noise'));
-    if contains(subD{ii}.name, 'electrical_noise')
+    tmp = d(ii);
+
+    if contains(tmp.name,'electrical_noise')
+        out(n) = generic_generator_template(tmp);
         out(n).type = 'electrical_noise';
         out(n).plotting_func = @mpqc.plot.electrical_noise;
+        [pathstr,out(n).name,ext] = fileparts(tmp.name);
+        n=n+1;
+    % else
+    %     disp('No files')
     end
-    % dir(data_dir,d.name)
-% if contains(d.name,'electrical_noise')
-%     disp(d.name)
-% end
 end
-% n =1;
-% if contains(d.name, 'electrical_noise')
-%     out(n).type = 'electrical_noise';
-%     out(n).plotting_func = @mpqc.plot.electrical_noise;
-% end
 
-n=1;
-% for ii=1:length(d)
-%     tmp = d(ii);
-%     if contains(tmp.name,'electrical_noise')
-%         % out(n) = generic_generator_template(tmp);
-%         out(n).type = 'electrical_noise';
-%         out(n).plotting_func = @mpqc.plot.electrical_noise;
-%         n=n+1;
-%     else
-%         disp('No files')
-%     end
-% end
-
+function out = generic_generator_template(t_dir)
+    out.full_path_to_data = fullfile(t_dir.folder,t_dir.name);
+    out.type = [];
+    out.plotting_func = [];
+    out.name = [];
+end
+for i = 1:length(out)
+    if contains(out(i).full_path_to_data, '.tif')
+        noiseData(:,:,i) = imread(out(i).full_path_to_data);
+    end
+end
 % t_im = single(imstack(:,:,ii));
 %         [n,x] = hist(t_im(:),100);
 %         a=area(x,n);
