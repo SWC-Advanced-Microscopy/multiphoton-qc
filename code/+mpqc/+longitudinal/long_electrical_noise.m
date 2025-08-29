abee3e6b5f2521af9b5fead1e61c07828d22a229
Isabell Whiteley <46966503+iwhiteley@users.noreply.github.com>
2025-08-29 15:49:48 +0100

    Added date labels for x axis on plots and gave brief description of the function

--- code/+mpqc/+longitudinal/long_electrical_noise.m
+++ code/+mpqc/+longitudinal/long_electrical_noise.m
@@ -1,6 +1,15 @@
 function [d,out,noiseData,fwhm] = long_electrical_noise(data_dir)
-
-
+% Longitudinal electrical noise plots
+%
+% mpqc.longitudinal.long_electrical_noise
+%
+% Purpose
+% Plots of the maximum value and FWHM of electrical noise for each channel 
+% with PMTs off. If there is significant change in electrical noise, the FWHM 
+% distributions will likely increase in value.
+%
+%
+% Isabell Whiteley, SWC AMF
 
 if nargin<1
     data_dir = pwd;
@@ -11,55 +20,69 @@ n=1;
 for ii=1:length(d)
     tmp = d(ii);
 
-    if contains(tmp.name,'electrical_noise')% && contains(tmp.name)
+    if contains(tmp.name,'electrical_noise')
         out(n) = generic_generator_template(tmp);
         out(n).type = 'electrical_noise';
         out(n).plotting_func = @mpqc.plot.electrical_noise;
+        out(n).date = string(tmp.date);
         [pathstr,out(n).name,ext] = fileparts(tmp.name);
         n=n+1;
-    % else
-    %     disp('No files')
+        % else
+        %     disp('No files')
     end
 end
 
-function out = generic_generator_template(t_dir)
-    out.full_path_to_data = fullfile(t_dir.folder,t_dir.name);
-    out.type = [];
-    out.plotting_func = [];
-    out.name = [];
-end
+    function out = generic_generator_template(t_dir)
+        out.full_path_to_data = fullfile(t_dir.folder,t_dir.name);
+        out.type = [];
+        out.plotting_func = [];
+        out.name = [];
+        out.date = [];
+    end
+
 for i = 1:length(out)
     if contains(out(i).full_path_to_data, '.tif')
         noiseData(:,:,i) = imread(out(i).full_path_to_data);
     end
 end
+
 noiseData = single(noiseData);
 % fwhm = 1;
 for t = 1:size(noiseData,3)
-% 
+    %
     t_im = noiseData(:,:,t);
     [n,x] = hist(t_im(:),100); % plots all data as histogram
-    figure;
-    a=area(n);
-    a.EdgeColor=[0,0,0.75];
-    a.FaceColor=[0.5,0.5,1];
-    a.LineWidth=2;
-    hold on
+    % figure;
+    % a=area(n);
+    % a.EdgeColor=[0,0,0.75];
+    % a.FaceColor=[0.5,0.5,1];
+    % a.LineWidth=2;
+    % hold on
     m = smoothdata(n,'gaussian',5);
     detail = interp1(x,m,[1:1000]);
-    b = plot(m);
-    b.LineWidth = 2;
-
-    hold off
+    % b = plot(m);
+    % b.LineWidth = 2;
+    % title(out(t).date)
+    % 
+    % hold off
     maxVal(t) = max(detail(:));
     halfMaxVal = maxVal(t)/2;
-    leftIndex = find(detail(:) >= halfMaxVal, 1, 'first'); 
+    leftIndex = find(detail(:) >= halfMaxVal, 1, 'first');
     rightIndex = find(detail(:) >= halfMaxVal, 1, 'last');
     fwhm(t) = rightIndex -leftIndex;
 end
-figure; 
+
+xlabels = {out.date};
+figure;
 subplot(2,1,1)
 plot(maxVal)
+xticks(1:length(xlabels))
+xticklabels(xlabels)
+title('Max value')
+
 subplot(2,1,2)
 plot(fwhm)
+xticks(1:length(xlabels))
+xticklabels(xlabels)
+title('FWHM')
 end
